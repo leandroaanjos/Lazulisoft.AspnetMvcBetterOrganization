f0ba1ab086edd9525cb4e0ee3ac0dbc7970b4ecd
Leandro Dos Anjos <laamaker@gmail.com>
2020-07-25 18:26:40 -0300

    Updated the bundles.cs file

--- src/Lazulisoft.AspnetMvcBetterOrganization.Web/App_Start/BundleConfig.cs
+++ src/Lazulisoft.AspnetMvcBetterOrganization.Web/App_Start/BundleConfig.cs
@@ -1,30 +1,26 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace Lazulisoft.AspnetMvcBetterOrganization.Web
 {
     public class BundleConfig
     {
-        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            // JS
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include("~/wwwroot/js/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/wwwroot/lib/jquery/dist/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/wwwroot/lib/jquery.validate/dist/jquery.validate.js",
+                        "~/wwwroot/lib/jquery.validate.unobtrusive/dist/jquery.validate.unobtrusive.js"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/wwwroot/lib/bootstrap/js/bootstrap.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+            // CSS
+            bundles.Add(new StyleBundle("~/bundles/sitecss").Include("~/wwwroot/css/site.css"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            BundleTable.EnableOptimizations = true;
         }
     }
 }
