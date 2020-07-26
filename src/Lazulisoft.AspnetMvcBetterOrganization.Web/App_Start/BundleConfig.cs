ee29bdac52633f410fb8c76ca628fa0c63c32652
Leandro Dos Anjos <laamaker@gmail.com>
2020-07-25 21:04:06 -0300

    Updated layout view file and bundle cs file

--- src/Lazulisoft.AspnetMvcBetterOrganization.Web/App_Start/BundleConfig.cs
+++ src/Lazulisoft.AspnetMvcBetterOrganization.Web/App_Start/BundleConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Optimization;
+﻿using System.Web;
+using System.Web.Optimization;
 
 namespace Lazulisoft.AspnetMvcBetterOrganization.Web
 {
@@ -17,10 +18,27 @@ namespace Lazulisoft.AspnetMvcBetterOrganization.Web
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/wwwroot/lib/bootstrap/js/bootstrap.js"));
 
-            // CSS
-            bundles.Add(new StyleBundle("~/bundles/sitecss").Include("~/wwwroot/css/site.css"));
+            // CSS            
+            bundles.Add(new StyleBundle("~/bundles/sitecss")
+                .Include("~/wwwroot/lib/bootstrap/css/bootstrap.css", new CssRewriteUrlTransformWrapper())
+                .Include("~/wwwroot/css/site.css"));
 
+            // To avoid change the file to .min version
+            BundleTable.Bundles.FileExtensionReplacementList.Clear();
+
+            // Enable the bundling and minification of bundle references
             BundleTable.EnableOptimizations = true;
+        }
+    }
+
+    // https://stackoverflow.com/questions/19765238/cssrewriteurltransform-with-or-without-virtual-directory
+    // Solution to: The problem appears when your application isn't hosted at the server root, but in a virtual directory
+    // Ex: /path/to/my/appRoot/
+    public class CssRewriteUrlTransformWrapper : IItemTransform
+    {
+        public string Process(string includedVirtualPath, string input)
+        {
+            return new CssRewriteUrlTransform().Process("~" + VirtualPathUtility.ToAbsolute(includedVirtualPath), input);
         }
     }
 }
